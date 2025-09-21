4e85b8540a6723dbd8e3057f62c5eeaa7e470f58
timothymulatya <timothymulatya@gmail.com>
2025-09-21 17:42:54 +0300

    push

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+
+  } else if (feet > 400 && feet <=2000) {
+    return 'That will be twenty bucks.'; 
+
+  } else if (feet > 2000 && feet <=2500){
+    return 'I will gladly take your thirty bucks.';
+
+  } else {
+    return 'No can do.';
+  
+  }
+
+
 }
 
 function ternaryCheckCity(){
